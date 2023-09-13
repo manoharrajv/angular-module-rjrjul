c0c071eebd586b0942b1a98fbe370a9b3678e084
manoharrajv <56024330+manoharrajv@users.noreply.github.com>
2023-09-13 09:18:47 +0530

    2 files modified, 4 added

---
+++ src/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `<app-server>
+  <app-server>`,
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
